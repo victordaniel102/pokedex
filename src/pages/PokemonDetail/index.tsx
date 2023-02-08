a4de497e72dcc8186130378e4c38a2ab20045ef2
victordaniel102 <daniel.victor9@hotmail.com>
2023-02-08 11:27:20 -0300

    first commit

---
+++ src/pages/PokemonDetail/index.tsx
@@ -0,0 +1,5 @@
+const PokemonDetail = () => {
+  return <></>;
+};
+
+export default PokemonDetail;
---
+++ src/routes/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Home from "../pages/Home";
+import Pokemons from "../pages/Pokemons";
+import PokemonDetail from "../pages/PokemonDetail";
+import NotFound from "../pages/NotFound";
+
+const AppRoutes: React.FC = () => {
+  return (
+    <Routes>
+      <Route path="/" element={Home()} />
+      <Route path="/pokemons" element={Pokemons()} />
+      <Route path="/pokemon/:id" element={PokemonDetail()} />
+      <Route path="*" element={NotFound()} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
