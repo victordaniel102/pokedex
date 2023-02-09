c621efeebd17dd990804df0f2a0992bad2b9e49d
victordaniel102 <daniel.victor9@hotmail.com>
2023-02-08 22:45:26 -0300

    feat: translation and theme context

---
+++ src/translation/index.ts
@@ -0,0 +1,9 @@
+import en from "./en.json";
+import ptbr from "./ptbr.json";
+
+export const dictionaryList = { ptbr, en };
+
+export const languageOptions = {
+  ptbr: "Português",
+  en: "English",
+};
